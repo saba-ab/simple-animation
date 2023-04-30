a597fb97cef3d9a41d4e6fefb9d21d00b42980a6
saba-ab <sabaabjandadze@gmail.com>
2023-05-01 02:48:08 +0400

    rewrited js code

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-window.onload = function () {
-  var audio = new Audio("./media/aphex-twin.mp3");
+var audio = new Audio("./media/aphex-twin.mp3");
+
+function playAudio() {
   audio.play();
-};
+}
+
+document.addEventListener("click", playAudio);
